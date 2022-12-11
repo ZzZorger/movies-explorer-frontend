c9b56d138248f25a80d49fded269d8a5ea7165a7
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-12-11 19:39:36 +0300

    Создание структуры компонентов

---
+++ src/components/Main/Promo/Promo.js
@@ -0,0 +1,8 @@
+export default function Promo() {
+  return(
+    <section className="promo">
+      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <img className="promo_image" alt="логотип практикума"></img>
+    </section>
+  )
+}
