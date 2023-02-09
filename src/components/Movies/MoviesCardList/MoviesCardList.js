913a6188b0d1902288b31ed43dfbbb41d7163930
Alexandr Yakushkin <jakushkinn@yandex.ru>
2023-02-09 22:09:38 +0300

    создание build

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -34,34 +34,19 @@ export default function MoviesCardList({
           )}
         </div>
         :
-        (filteredMovies.length === 0 ?
-          <div className="card-list__cards">
-            {savedMovies.map((movie) => {
-              return (
-                <MoviesCard
-                  key={movie.id || movie._id}
-                  movie={movie}
-                  onLikeButton={onLikeButton}
-                  onDislikeButton={onDislikeButton}
-                  onSavedPageFlag={onSavedPageFlag}
-                />)
-            }
-            )}
-          </div> :
-          <div className="card-list__cards">
-            {filteredMovies.map((movie) => {
-              return (
-                <MoviesCard
-                  key={movie.id || movie._id}
-                  movie={movie}
-                  onLikeButton={onLikeButton}
-                  onDislikeButton={onDislikeButton}
-                  onSavedPageFlag={onSavedPageFlag}
-                />)
-            }
-            )}
-          </div>
-        )
+        <div className="card-list__cards">
+          {savedMovies.map((movie) => {
+            return (
+              <MoviesCard
+                key={movie.id || movie._id}
+                movie={movie}
+                onLikeButton={onLikeButton}
+                onDislikeButton={onDislikeButton}
+                onSavedPageFlag={onSavedPageFlag}
+              />)
+          }
+          )}
+        </div>
       }
       <div className="card-list__next">
         {!addMoviesEnable && <button className="card-list__button transition" onClick={handleAddMovies}>Ещё</button>}
